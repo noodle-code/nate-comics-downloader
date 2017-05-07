7085254cb39332b620bde14e4c4d2f54aa412644
noodlecode <aldrinthomaslab@gmail.com>
2017-05-07 16:40:42 +0800

    Added single or multiple chapter detection

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
 import os
+import re
 import requests
 import urllib.request
 
@@ -35,6 +36,17 @@ def getbaseurl(url):
     base_url = rawurl.scheme + '://' + rawurl.netloc
     return
 
+def isLinkSpecific(url):
+    pattern = re.compile(r'detail.php')
+
+    return pattern.search(url)
+
+def parseUrlToObject(url):
+    return_data = requests.get(url)
+    data = return_data.text
+    soupObject = BeautifulSoup(data, 'html.parser')
+    return soupObject
+
 def getchapters(soup):
     global chapters
 
@@ -51,9 +63,10 @@ def getImage(url):
     global current_comic
     ch_title = ''
 
-    raw = requests.get(url)
-    rawData = raw.text
-    cur_chap = BeautifulSoup(rawData, "html.parser")
+    # raw = requests.get(url)
+    # rawData = raw.text
+    # cur_chap = BeautifulSoup(rawData, "html.parser")
+    cur_chap = parseUrlToObject(url)
 
     ch_info = cur_chap.find('div', { "class" : "tvi_episodeInfo" })
     ch_num = ch_info.find('span', { "class" : "tvi_episodeNum" })
@@ -85,6 +98,31 @@ def getNextPage(soup):
 
     return nextPage
 
+def dlImageFromLink(url):
+    print("Starting chapter download...")
+    getImage(url)
+    return
+
+def dlChaptersFromLink(url):
+    global nextPage
+
+    print("Scraping for available chapters...")
+
+    while nextPage:
+
+        soup = parseUrlToObject(url)
+        getchapters(soup)
+
+        nextPage = getNextPage(soup)
+
+        if nextPage:
+            url = base_url + nextPage
+
+    print("Starting download of chapter images...")
+
+    for chapter in reversed(chapters):
+        getImage(base_url + chapter)
+
 # Init
 checkDirectory(comic_dir)
 
@@ -94,25 +132,9 @@ getbaseurl(url)
 folder = input("Folder name for downloads: ")
 checkComicFolder(folder)
 
-print("Scraping for available chapters...")
-
-while nextPage:
-
-    return_data = requests.get(url)
-    data = return_data.text
-
-    soup = BeautifulSoup(data, 'html.parser')
-
-    getchapters(soup)
-
-    nextPage = getNextPage(soup)
-
-    if nextPage:
-        url = base_url + nextPage
-
-print("Starting download of chapter images...")
-
-for chapter in reversed(chapters):
-    getImage(base_url + chapter)
+if isLinkSpecific(url):
+    dlImageFromLink(url)
+else:
+    dlChaptersFromLink(url)
 
 print('Scraping finished')
